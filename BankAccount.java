8092c76d29cb5a72b246e6e10bf992eb7c493434
Mohammad Hammad <k253110@nu.edu.pk>
2026-02-10 15:42:43 +0500

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,32 @@
+package Lab02;
+
+public class BankAccount {
+    int AccountNumber;
+    String AccountName;
+    double Balance;
+
+    BankAccount(int Pnumber, String Pname){
+        this.AccountNumber = Pnumber;
+        this.AccountName = Pname;
+    }
+
+    BankAccount(int Pnumber, String Pname, double Pbalance){
+        this.AccountNumber = Pnumber;
+        this.AccountName = Pname;
+        this.Balance = Pbalance;
+    }
+
+    public void displayAccDetails(){
+        System.out.println("Account Number : " + AccountNumber);
+        System.out.println("Account Name : " + AccountName);
+        System.out.println("Balance : " + Balance);
+    }
+
+    public static void main(String[] args){
+        BankAccount acc1 = new BankAccount(10, "Ali");
+        acc1.displayAccDetails();
+
+        BankAccount acc2 = new BankAccount(20, "Umar", 50000);
+        acc2.displayAccDetails();
+    }
+}
